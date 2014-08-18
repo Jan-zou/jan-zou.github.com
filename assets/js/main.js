d396e3e8dd675beba51b3ecf428876037bc78e7e
Jan Zou <zoujlin@gmail.com>
2014-08-18 19:46:10 +0800

    add totop,new post

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+// back to top
+window.onscroll = function(){
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+    var div_up = document.getElementById("uptop");
+    t > 300 ? div_up.style.display="block" : div_up.style.display="none";
+}
+
+function scroTop(){
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
